d727e2b4882ff44fa92096ab99ead1c3203e8ce6
Harrison Edwards <harrisonedwards@Harrisons-MacBook-Pro.local>
2020-03-14 17:37:25 -0700

    code cleanup

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -7,21 +7,6 @@ import App from '../client/src/components/App';
 configure({ adapter: new Adapter() });
 
 describe('Unit Tests', () => {
-  // jest.mock('axios', () => {
-  //   const tasks = [
-  //     {
-  //       isComplete: false,
-  //       _id: '5e14daea497a3a48c5bd4612',
-  //       task: 'Write some tests!',
-  //       date: '2020-01-07T19:24:26.332Z',
-  //       __v: 0,
-  //     },
-  //   ];
-
-  //   return {
-  //     get: jest.fn(() => Promise.resolve(tasks)),
-  //   };
-  // });
 
   test('should render the app component on the screen', () => {
     const wrapper = shallow(<App />);
