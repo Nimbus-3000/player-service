1d906dc19576e06c4fed02f2faeeadc52c6c4c09
Eric Liu <eric@mikes-MBP-3.attlocal.net>
2020-04-13 15:11:07 -0700

    Stress testing with K6 and New Relic

--- k6.js
+++ k6.js
@@ -4,7 +4,7 @@ import { check, sleep } from 'k6';
 export let options = {
   stages: [
     { duration: '1s', target: 100 },
-    { duration: '60s', target: 1000 }
+    { duration: '20s', target: 1000 }
   ]
 };
 
@@ -12,7 +12,7 @@ export default function() {
   let res = http.get(`http://localhost:3001/api/songs/${Math.floor(Math.random() * 10000000) + 1}`);
   check(res, {
     'status was 200': r => r.status == 200,
-    'transaction time OK': r => r.timings.duration < 200,
+    'transaction time OK': r => r.timings.duration < 2000,
   });
-  sleep(1);
+  sleep(0.1);
 }
--- postgres/server.js
+++ postgres/server.js
@@ -1,8 +1,8 @@
-require('newrelic');
+// require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const pg = require('../postgres/index.js');
+const { postSong, getSong, getSongComments, putSong, deleteSong } = require('./db.js');
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,34 +11,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.listen(port, () => console.log(`listening on port ${port}.`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // create single new song
-app.post('/api/songs', (req, res) => {
-  pg.postSong(req.data)
+app.post('/api/songs', (req, res, next) => {
+  postSong(req.data)
     .then(res.sendStatus(200))
+    .catch(next);
 });
 
 // read song with given id
-app.get('/api/songs/:id', (req, res) => {
-  pg.getSong(req.params.id)
+app.get('/api/songs/:id', (req, res, next) => {
+  getSong(req.params.id)
     .then(data => res.status(200).send(data))
+    .catch(next);
 });
 
 // read all comments with a given songid
-app.get('/api/comments/songid/:id', (req, res) => {
-  pg.getSongComments(req.params.id)
+app.get('/api/comments/songid/:id', (req, res, next) => {
+  getSongComments(req.params.id)
     .then(data => res.status(200).send(data))
+    .catch(next);
 });
 
 // update song with same id as req.data
-app.put('/api/songs', (req, res) => {
-  pg.putSong(req.data)
+app.put('/api/songs', (req, res, next) => {
+  putSong(req.data)
     .then(res.sendStatus(200))
+    .catch(next);
 });
 
 // delete song with given id
-app.delete('/api/songs/:_id', (req, res) => {
-  pg.deleteSong(req.params.id)
+app.delete('/api/songs/:_id', (req, res, next) => {
+  deleteSong(req.params.id)
     .then(res.sendStatus(200))
+    .catch(next);
 });
